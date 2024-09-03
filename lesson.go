b371e4cf2d3ad4709a8a99fad40f8a5b13d09d8b
joyeajin <dpwls31200@gmail.com>
2024-09-03 14:30:16 +0900

    조건문 if, else if, else

--- lesson.go
+++ lesson.go
@@ -2,11 +2,25 @@ package main
 
 import "fmt"
 
-func main(){
-	var number uint8 =29
-	// test1 = "Hello shop2"
-	// test1 = "shop3"
-	number = number + 100
-	fmt.Println(number)
-}
+func main() {
+	age := 15
 
+	if age >= 18 {
+		fmt.Println("군대갈 수 있음")
+	} else if age >= 14 {
+		fmt.Println("학도병에 지원하세요")
+	} else {
+		fmt.Println("군대갈 수 없음")
+	}
+
+	// if age >= 18 {
+	// 	fmt.Println("군대갈 수 있음")
+	// }
+	// if age >= 14 && age < 18 {
+	// 	fmt.Println("학도병에 지원하세요")
+	// }
+	// if age < 14 {
+	// 	fmt.Println("군대갈 수 없음")
+	// }
+
+}
