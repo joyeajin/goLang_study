46662a7f66dbc4a3ca1410b26bd3d5818a87d2be
joyeajin <dpwls31200@gmail.com>
2024-10-21 11:46:01 +0900

    true, false

--- lesson.go
+++ lesson.go
@@ -5,10 +5,9 @@ import (
 )
 
 func main() {
+	x := '7'
+	y := '3'
+	answer := x < y
 
-	var num1 int = 9
-	var num2 int = 2
-	answer := num1 % num2
-
-	fmt.Printf("%d", answer)
+	fmt.Printf("%t", answer)
 }
